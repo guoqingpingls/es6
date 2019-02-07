e9847fb44598518722019f5b5b8bb7990d75e6bd
guoqingpingls <1151614788@qq.com>
2019-02-07 15:04:59 +0800

    code commit

--- app/js/class/regex.js
+++ app/js/class/regex.js
@@ -23,7 +23,7 @@
 }
 
 {
-  // u修饰符  正则处理
+  // u修饰符  正则处理 unicode u 是正则在处理unicode字符的时候的特征值
   // 没加 会吧后面的当成四个字节 2+2字节 两个字符
   // 加了 会把后面的四个字节当成一个字符
   console.log('u-1:', /^\uD83D/.test('\uD83D\uDc2A'))   // true
@@ -33,12 +33,16 @@
   console.log(/\u{61}/.test('a'))   // false
   console.log(/\u{61}/u.test('a'))   // true
 
-  // .字符
+  // .字符  <0xf的字符
   console.log(`\u{20bb7}`);
   let s = '𠮷'
   // 字符大于俩个字节需要加上u修饰符
   console.log('u: ', /^.$/.test(s));    // false
   console.log('u-1: ', /^.$/u.test(s));   // true
 
-  console.log('test: ', /𠮷{2}/.test('𠮷𠮷'))
+  console.log('test: ', /𠮷{2}/.test('𠮷𠮷')) // false
+  console.log('test-2: ', /𠮷{2}/u.test('𠮷𠮷')) // true
+  // 如果处理的字符串中 正则表达式中，有大于两个字节的字符
+  // .可以匹配到任意字符（必须小于等于两个字节）
+  // （回车符，换行符，行分隔符，段分隔符，这些.是不能识别的，这时候需要s修饰符，暂未实现）
 }
--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,2 @@
-import './class/regex.js'
+import 'babel-polyfill'
+import './class/string.js'
